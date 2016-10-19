9e5264a250b84fbf4bd0090ddba8cc125acaae37
Nick Doiron <ndoiron@mapmeld.com>
2016-10-18 23:51:22 -0400

    initial queries

--- server.py
+++ server.py
@@ -1,15 +1,38 @@
-# pip install web.py
+# pip install web.py py2neo
+# connect to localhost:7474 to set your password
+from py2neo import Graph, Node, Relationship
+g = Graph(user="neo4j", password="admin")
+
 import web
 render = web.template.render('templates/')
 
+import json
+
 urls = (
   '/', 'home',
+  '/person', 'person',
   '/bootstrap.min.css', 'bootstrap'
 )
 
 class home:
     def GET(self):
         return render.home()
+
+class person:
+    def GET(self):
+        # simple query, no regex or dedupe
+        searchQuery = web.input().search
+        person = g.find_one("Officer", "name", searchQuery)
+        # return json.dumps(person)
+
+        # multiple with regexp
+        searchQuery = searchQuery.replace(' ', '.*')
+        results = g.run("MATCH (p:Officer) WHERE p.name =~ {name} RETURN p", name=searchQuery)
+        people = []
+        for person in results:
+            people.append(person)
+        return json.dumps(people)
+
 
 class bootstrap:
     def GET(self):
